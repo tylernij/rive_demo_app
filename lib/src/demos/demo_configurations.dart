7d8ddcd0e7241a434e71bd27074573018172555f
Tyler Nijmeh <tylernij@gmail.com>
2026-05-20 15:49:29 -0500

    configs: Separate list from definitions
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/src/demos/demo_configurations.dart
+++ lib/src/demos/demo_configurations.dart
@@ -7,234 +7,238 @@ import 'package:rive_demo_app/src/demos/treasure_chest/treasure_chest.dart';
 import 'package:rive_demo_app/src/model/safe_area_layout.dart';
 import 'package:rive_demo_app/src/screens/rive_harness/rive_harness.dart';
 
-/// The demo configurations.
+/// The demo configurations, ordered.
 final demoConfigurations = <DemoConfiguration>[
-  // Avatar Creator Demo.
-  // const DemoConfiguration(
-  //   displayName: 'Avatar Creator',
-  //   harness: RiveHarness(
-  //     entryPoint: AvatarCreator(),
-  //     showBackButton: false, // We handle this internally.
-  //   ),
-  // ),
-
-  // Treasure Chest Demo.
-  const DemoConfiguration(
-    displayName: 'Treasure Chest',
-    description: 'Set my description, please!',
-    harness: RiveHarness(entryPoint: TreasureChest()),
-  ),
-
-  // Responsive Layouts Demo.
-  // const DemoConfiguration(
-  //   displayName: 'Responsive Layouts',
-  //   harness: RiveHarness(
-  //     entryPoint: GenericRive(
-  //       assetPath: 'assets/riv/responsive_layouts/layouts_demo.riv',
-  //     ),
-  //   ),
-  // ),
-
-  // Duelist.
-  const DemoConfiguration(
-    displayName: 'Duelist',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: Duelist(),
-      showBackButton: false,
-    ),
-  ),
-
-  // Feathering Button Demo.
-  DemoConfiguration(
-    displayName: 'Feathering Button',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/feathering_button/push_button.riv',
-        dataBind: DataBind.auto(),
-      ),
-    ),
-  ),
-
-  // Feathering Coin Demo.
-  const DemoConfiguration(
-    displayName: 'Feathering Coin',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/feathering_coin/coin_demo_feathering.riv',
-        layoutScaleFactor: 0.5,
-      ),
-    ),
-  ),
-
-  // Data Visualization Demo.
-  const DemoConfiguration(
-    displayName: 'Data Viz',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: DataViz(),
-    ),
-  ),
-
-  // Ad Widget Demo.
-  DemoConfiguration(
-    displayName: 'Ad Widget',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/ad_widget/amazon_ad-demo_2026.riv',
-        dataBind: DataBind.auto(),
-      ),
-    ),
-  ),
-
-  // RTL Language Layout Demo.
-  DemoConfiguration(
-    displayName: 'RTL Language Layout',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      useSafeArea: true,
-      safeAreaLayouts: const {SafeAreaLayout.bottom},
-      backgroundColor: const Color(0xFFCCCCCC),
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/rtl_language_layout/rtllanguagelayout.riv',
-        dataBind: DataBind.auto(),
-      ),
-    ),
-  ),
-
-  // Liquid Download Demo.
-  DemoConfiguration(
-    displayName: 'Liquid Download',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/liquid_download/liquid_download.riv',
-        dataBind: DataBind.auto(),
-        fit: Fit.contain,
-      ),
-    ),
-  ),
-
-  // Scrollytelling Demo.
-  const DemoConfiguration(
-    displayName: 'Scrollytelling',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath:
-            'assets/riv/scrollytelling/scrolly-telling-vertical-responsive.riv',
-      ),
-    ),
-  ),
-
-  // Scrollytelling Raster Demo.
-  const DemoConfiguration(
-    displayName: 'Scrollytelling Raster',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath:
-            'assets/riv/scrollytelling_raster/scrolly-telling-raster.riv',
-      ),
-    ),
-  ),
-
-  // Car Widget Demo.
-  const DemoConfiguration(
-    displayName: 'Car Widget',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/car_widget/car_widgets_v01.riv',
-      ),
-    ),
-  ),
-
-  // Fintech Demo.
-  DemoConfiguration(
-    displayName: 'Fintech',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/fintech/fintech_data_viz_v03.riv',
-        fit: Fit.layout,
-        dataBind: DataBind.auto(),
-        layoutScaleFactor: 0.5,
-      ),
-    ),
-  ),
-
-  // Game Character Demo.
-  const DemoConfiguration(
-    displayName: 'Game Character',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/game_character/game_character_demo.riv',
-      ),
-    ),
-  ),
-
-  // Car Component Demo.
-  const DemoConfiguration(
-    displayName: 'Car Component',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/car_component/car_component_demo.riv',
-      ),
-    ),
-  ),
-
-  // Music Player Demo.
-  const DemoConfiguration(
-    displayName: 'Music Player',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/music_player/music_player.riv',
-      ),
-    ),
-  ),
-
-  // Slot Machine Demo.
-  DemoConfiguration(
-    displayName: 'Slot Machine',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/slot_machine/slot_machine.riv',
-        dataBind: DataBind.auto(),
-      ),
-    ),
-  ),
-
-  // Inventory Demo.
-  const DemoConfiguration(
-    displayName: 'Inventory',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/inventory/inventory_demo_test_v2.riv',
-      ),
-    ),
-  ),
-
-  // Sports Broadcast Demo.
-  DemoConfiguration(
-    displayName: 'Sports Broadcast',
-    description: 'Set my description, please!',
-    harness: RiveHarness(
-      entryPoint: GenericRive(
-        assetPath: 'assets/riv/sports_broadcast/sports_broadcaat.riv',
-        dataBind: DataBind.auto(),
-      ),
-    ),
-  ),
+  treasureChestConfig,
+  duelistConfig,
+  featheringButtonConfig,
+  featheringCoinConfig,
+  dataVizConfig,
+  adWidgetConfig,
+  rtlLanguageLayoutConfig,
+  liquidDownloadConfig,
+  scrollytellingConfig,
+  scrollytellingRasterConfig,
+  carWidgetConfig,
+  fintechConfig,
+  gameCharacterConfig,
+  carComponentConfig,
+  musicPlayerConfig,
+  slotMachineConfig,
+  inventoryConfig,
+  sportsBroadcastConfig,
 ];
+
+/// ============================== CONFIGURATIONS ==============================
+/// Define the demo configurations here.
+
+/// Treasure Chest Demo.
+const treasureChestConfig = DemoConfiguration(
+  displayName: 'Treasure Chest',
+  description: 'Set my description, please!',
+  harness: RiveHarness(entryPoint: TreasureChest()),
+);
+
+/// Duelist Demo.
+const duelistConfig = DemoConfiguration(
+  displayName: 'Duelist',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: Duelist(),
+    showBackButton: false,
+  ),
+);
+
+/// Feathering Button Demo.
+final featheringButtonConfig = DemoConfiguration(
+  displayName: 'Feathering Button',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/feathering_button/push_button.riv',
+      dataBind: DataBind.auto(),
+    ),
+  ),
+);
+
+/// Feathering Coin Demo.
+const featheringCoinConfig = DemoConfiguration(
+  displayName: 'Feathering Coin',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/feathering_coin/coin_demo_feathering.riv',
+      layoutScaleFactor: 0.5,
+    ),
+  ),
+);
+
+/// Data Viz Demo.
+const dataVizConfig = DemoConfiguration(
+  displayName: 'Data Viz',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: DataViz(),
+  ),
+);
+
+/// Ad Widget Demo.
+final adWidgetConfig = DemoConfiguration(
+  displayName: 'Ad Widget',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/ad_widget/amazon_ad-demo_2026.riv',
+      dataBind: DataBind.auto(),
+    ),
+  ),
+);
+
+/// RTL Language Layout Demo.
+final rtlLanguageLayoutConfig = DemoConfiguration(
+  displayName: 'RTL Language Layout',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    useSafeArea: true,
+    safeAreaLayouts: const {SafeAreaLayout.bottom},
+    backgroundColor: const Color(0xFFCCCCCC),
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/rtl_language_layout/rtllanguagelayout.riv',
+      dataBind: DataBind.auto(),
+    ),
+  ),
+);
+
+/// Liquid Download Demo.
+final liquidDownloadConfig = DemoConfiguration(
+  displayName: 'Liquid Download',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/liquid_download/liquid_download.riv',
+      dataBind: DataBind.auto(),
+      fit: Fit.contain,
+    ),
+  ),
+);
+
+/// Scrollytelling Demo.
+const scrollytellingConfig = DemoConfiguration(
+  displayName: 'Scrollytelling',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath:
+          'assets/riv/scrollytelling/scrolly-telling-vertical-responsive.riv',
+    ),
+  ),
+);
+
+/// Scrollytelling Raster Demo.
+const scrollytellingRasterConfig = DemoConfiguration(
+  displayName: 'Scrollytelling Raster',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/scrollytelling_raster/scrolly-telling-raster.riv',
+    ),
+  ),
+);
+
+/// Car Widget Demo.
+const carWidgetConfig = DemoConfiguration(
+  displayName: 'Car Widget',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/car_widget/car_widgets_v01.riv',
+    ),
+  ),
+);
+
+/// Fintech Demo.
+final fintechConfig = DemoConfiguration(
+  displayName: 'Fintech',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/fintech/fintech_data_viz_v03.riv',
+      fit: Fit.layout,
+      dataBind: DataBind.auto(),
+      layoutScaleFactor: 0.5,
+    ),
+  ),
+);
+
+/// Game Character Demo.
+const gameCharacterConfig = DemoConfiguration(
+  displayName: 'Game Character',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/game_character/game_character_demo.riv',
+    ),
+  ),
+);
+
+/// Car Component Demo.
+const carComponentConfig = DemoConfiguration(
+  displayName: 'Car Component',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/car_component/car_component_demo.riv',
+    ),
+  ),
+);
+
+/// Music Player Demo.
+const musicPlayerConfig = DemoConfiguration(
+  displayName: 'Music Player',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/music_player/music_player.riv',
+    ),
+  ),
+);
+
+/// Slot Machine Demo.
+final slotMachineConfig = DemoConfiguration(
+  displayName: 'Slot Machine',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/slot_machine/slot_machine.riv',
+      dataBind: DataBind.auto(),
+    ),
+  ),
+);
+
+/// Inventory Demo.
+const inventoryConfig = DemoConfiguration(
+  displayName: 'Inventory',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/inventory/inventory_demo_test_v2.riv',
+    ),
+  ),
+);
+
+/// Sports Broadcast Demo.
+final sportsBroadcastConfig = DemoConfiguration(
+  displayName: 'Sports Broadcast',
+  description: 'Set my description, please!',
+  harness: RiveHarness(
+    entryPoint: GenericRive(
+      assetPath: 'assets/riv/sports_broadcast/sports_broadcaat.riv',
+      dataBind: DataBind.auto(),
+    ),
+  ),
+);
+
+/// ============================== CONFIGURATIONS ==============================
 
 /// The configuration profile for each demo.
 class DemoConfiguration {
